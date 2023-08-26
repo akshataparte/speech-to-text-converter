c9c3c953b4490c12f07e483106ee87a17001e418
akshataparte <142744300+akshataparte@users.noreply.github.com>
2023-08-26 15:53:59 +0530

    speech converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import react, { useState } from 'react';
+import useClipboard from 'react-use-clipboard';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
 
 function App() {
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy);
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  const startListening = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: 'en-IN',
+    });
+  };
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="big-container">
+        <h2>Speech To Text Converter </h2>
+        <div className="container">
+          {/* in order to copy user has to click on the textarea and then press copy button  */}
+          <div
+            className="main-content"
+            onClick={() => setTextToCopy(transcript)}
+          >
+            {transcript}
+          </div>
+
+          <div className="btn-style">
+            <button onClick={setCopied} className="btn">
+              {isCopied ? 'Copied' : 'Copy to clipboard'}
+            </button>
+
+            <button onClick={startListening} className="btn">
+              Start Listening
+            </button>
+            <button onClick={SpeechRecognition.stopListening} className="btn">
+              Stop Listening
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+// apna website brower ko support kar raha hai ki nai vo browserSupportsSpeechRecognition isse pata chalega
